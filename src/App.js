8f491612b995fa59614e973d49bbf51dbc0cf309
ZhBauyrzhan <zhnbauyrzhan@gmail.com>
2022-06-21 14:12:30 +0600

    Первая попытка

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Photo } from './Photo';
+import 'bootstrap/dist/css/bootstrap.css';
+import './style.css';
+
+const imageInfo = [
+	{
+		src: 'https://source.unsplash.com/gYl-UtwNg_I/1500x1500',
+		text: 'HEY',
+	},
+	{
+		src: 'https://source.unsplash.com/rFKUFzjPYiQ/1500x1500',
+		text: "LET'S",
+	},
+	{
+		src: 'https://images.unsplash.com/photo-1465188162913-8fb5709d6d57?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&cs=tinysrgb&w=1500&h=1500&fit=crop&s=967e8a713a4e395260793fc8c802901d',
+		text: 'GIVE',
+	},
+	{
+		src: 'https://source.unsplash.com/ITjiVXcwVng/1500x1500',
+		text: 'ALL',
+	},
+	{
+		src: 'https://source.unsplash.com/3MNzGlQM7qs/1500x1500',
+		text: 'YOU CAN',
+	},
+];
+const App = () => {
+	return (
+		<div className="App d-flex flex-column flex-xl-row align-items-stretch h-100">
+			{imageInfo.map((imageInfo) => (
+				<Photo
+					key={imageInfo.src}
+					src={imageInfo.src}
+					text={imageInfo.text}
+				/>
+			))}
+		</div>
+	);
+};
+export default App;
---
+++ src/Photo.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+// import 'bootstrap/dist/css/bootstrap.css';
+export const Photo = (props) => {
+	return (
+		<div
+			className="d-flex photo col align-self-stretch justify-content-center align-items-center"
+			style={{
+				backgroundImage: `url( ${props.src} )`,
+				backgroundPosition: 'center',
+			}}
+		>
+			<h1 className="text align-self-center"> {props.text} </h1>
+		</div>
+	);
+};
